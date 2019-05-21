4b6960712ea1f90271397fd9c67d60f0847e6a5b
Strozensky <rstrozensky@automationdirect.com>
2019-05-21 16:24:47 -0400

    Initial commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+    <div class="container">
+        <button><a href="https://www.youtube.com/automationdirect">Visit Channel</a></button>
+        <button><a href="https://www.automationdirect.com/adc/home/home"><img src="/imgs/white_adc_logo.png" alt="ADC Logo"> Visit AutomationDirect.com</a></button>
+    </div>
+    </body>
+</html>
