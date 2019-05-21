947c283e07334c9e94306e26ee8f137ee24bb1d9
Strozensky <rstrozensky@automationdirect.com>
2019-05-21 16:38:53 -0400

    Added YouTube logo

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <div class="container">
-        <button><a href="https://www.youtube.com/automationdirect">Visit Channel</a></button>
+        <button><a href="https://www.youtube.com/automationdirect"><img src="/imgs/yt_icon_rgb.png" alt="YouTube Play Button Logo"> Visit Channel</a></button>
         <button><a href="https://www.automationdirect.com/adc/home/home"><img src="/imgs/white_adc_logo.png" alt="ADC Logo"> Visit AutomationDirect.com</a></button>
     </div>
     </body>
