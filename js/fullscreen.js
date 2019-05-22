375dcc03a5d3e1c3fadd360676dd81d0c10c7aac
Strozensky <rstrozensky@automationdirect.com>
2019-05-22 10:11:33 -0400

    Fixed fullscreen button

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,39 +1,43 @@
+window.onload = function () {
+  document.getElementById('fullscreen-button')
+      .addEventListener("click", function () { toggleFullscreen(); });
+}
 var doc = document.documentElement;
-var fullscreenStatus = "opened";
+var fullscreenStatus = "closed";
 
 function openFullscreen() {
-    if (doc.requestFullscreen) {
-        doc.requestFullscreen();
-    } else if (doc.mozRequestFullScreen) {
-        doc.mozRequestFullScreen();
-    } else if (doc.webkitRequestFullscreen) {
-        doc.webkitRequestFullscreen();
-    } else if (doc.msRequestFullscreen) {
-        doc.msRequestFullscreen();
-    }
-    fullscreenStatus = "opened";
+  if (doc.requestFullscreen) {
+    doc.requestFullscreen();
+  } else if (doc.mozRequestFullScreen) {
+    doc.mozRequestFullScreen();
+  } else if (doc.webkitRequestFullscreen) {
+    doc.webkitRequestFullscreen();
+  } else if (doc.msRequestFullscreen) {
+    doc.msRequestFullscreen();
+  }
+  fullscreenStatus = "opened";
 }
 
 function closeFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-    }
-    fullscreenStatus = "closed";
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+  fullscreenStatus = "closed";
 }
 
 function toggleFullscreen() {
-    var fullscreenButton = document.getElementById('fullscreen-button');
-    if (fullscreenStatus == "opened") {
-        closeFullscreen();
-        fullscreenButton.src="/open-iconic/svg/fullscreen-enter.svg";
-    } else {
-        openFullscreen();
-        fullscreenButton.src="/open-iconic/svg/fullscreen-exit.svg";
-    }
+  var fullscreenButton = document.getElementById('fullscreen-button');
+  if (fullscreenStatus == "opened") {
+    closeFullscreen();
+    fullscreenButton.src="open-iconic/svg/fullscreen-enter.svg";
+  } else {
+    openFullscreen();
+    fullscreenButton.src="open-iconic/svg/fullscreen-exit.svg";
+  }
 }
