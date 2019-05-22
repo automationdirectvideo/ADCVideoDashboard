c7ce73b4be7c695b507d15015eafb07fd2ede376
Strozensky <rstrozensky@automationdirect.com>
2019-05-22 08:49:47 -0400

    Deleted php files and created index.html

--- footer.php
+++
@@ -1,6 +0,0 @@
-    <div class="container">
-        <button><a href="https://www.youtube.com/automationdirect"><img src="/imgs/yt_icon_rgb.png" alt="YouTube Play Button Logo"> Visit Channel</a></button>
-        <button><a href="https://www.automationdirect.com/adc/home/home"><img src="/imgs/white_adc_logo.png" alt="ADC Logo"> Visit AutomationDirect.com</a></button>
-    </div>
-    </body>
-</html>
