577538fbdd912634d4d0196e7f9764c0ea410e73
Strozensky <rstrozensky@automationdirect.com>
2019-05-23 13:37:32 -0400

    Add debug statement to callDataAPI

--- js/main.js
+++ js/main.js
@@ -181,6 +181,7 @@ function callAnalyticsAPI(request, callback) {
 
 // Calls the Data API with a request and returns response to callback
 function callDataAPI(request, callback) {
+  console.log("Request", request);
   gapi.client.youtube.channels.list(request)
   .then(response => {
     console.log("Response", response);
