52ec1530e03232fb8c725dc78afadc8fd4a7e8fb
Strozensky <rstrozensky@automationdirect.com>
2019-05-23 14:58:24 -0400

    Moved call API functions to top of document

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// Calls the Analytics API with a request and returns response to callback
+function callAnalyticsAPI(request, callback) {
+  gapi.client.youtubeAnalytics.reports.query(request)
+    .then(response => {
+      console.log("Response", response);
+      callback(response);
+    })
+    .catch(err => {
+      console.error("Analytics API call error", err);
+    });
+}
+
+// Calls the Data API for channels with a request and returns response to callback
+function callDataAPIChannels(request, callback) {
+  gapi.client.youtube.channels.list(request)
+    .then(response => {
+      console.log("Response", response);
+      callback(response);
+    })
+    .catch(err => {
+      console.error("Data API call error", err);
+    });
+}
+
+// Calls the Data API for playlists with a request and returns response to callback
+function callDataAPIPlaylists(request, callback) {
+  gapi.client.youtube.playlistItems.list(request)
+    .then(response => {
+      console.log("Response", response);
+      callback(response);
+    });
+}
+
 // Display channel data
 function showChannelData(data) {
   const channelData = document.getElementById("channel-data");
