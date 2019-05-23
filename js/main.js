c4dd8735caccedd0b8f4863bd75d9867d06aa42a
Strozensky <rstrozensky@automationdirect.com>
2019-05-23 15:22:49 -0400

    Create handlesImpressionsForLast function

--- js/main.js
+++ js/main.js
@@ -141,16 +141,9 @@ function handleSubscribersGained(response) {
   }
 }
 
-// Request impressions in the last numDays days from Analytics API
-function requestImpressionsForLast(numDays) {
-  var todayDate = getTodaysDate();
-  var startDate = getDateFromDaysAgo(numDays);
-  var request = {
-    "endDate": todayDate,
-    "ids": "channel==MINE",
-    "metrics": "cardImpressions,cardTeaserImpressions,annotationsImpressions",
-    "startDate": startDate
-  };
-  callAnalyticsAPI(request, handleImpressionsForLast);
+//Handles impressions response from Analytics API
+function handlesImpressionsForLast() {
+  if (response) {
+    console.log("Response from Analytics API received");
+  }
 }
-
