ebc4a9a372a2b118ae64f9b0329aa9e396ac2865
Strozensky <rstrozensky@automationdirect.com>
2019-05-23 14:55:32 -0400

    Add getYouTubeDateFormat function

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,12 @@
+// Get a date in the form YYYY-MM-DD
+function getYouTubeDateFormat(date) {
+  var dd = String(date.getDate()).padStart(2, "0");
+  var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = date.getFullYear();
+  today = year + "-" + month + "-" + day;
+  return today;
+}      
+
 // Add commas to number
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
