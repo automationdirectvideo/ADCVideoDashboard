dbeb19c388e703e882206a6a6aa5297170b2c5b0
Strozensky <rstrozensky@automationdirect.com>
2019-05-31 11:17:11 -0400

    Update playlist api call parameters

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function testAPICalls() {
   requestVideoViewsByTrafficSource(thirtyDaysAgo, todayDate, "mXcDYoz1iMo");
   // requestViewsByDeviceType(joinDate, todayDate);
   // requestViewsByTrafficSource(joinDate, todayDate);
-  requestVideoPlaylist(uploadsPlaylistId, 1000);
+  requestVideoPlaylist(uploadsPlaylistId, 50);
 }
 
 // Display channel data
