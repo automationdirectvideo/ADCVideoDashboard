3d802a75f83d2d12496e0c49617b5a74d3e4322c
Strozensky <rstrozensky@automationdirect.com>
2019-06-10 16:28:01 -0400

    Add videos to non organic videos list

--- js/handle.js
+++ js/handle.js
@@ -77,7 +77,9 @@ function handleMostWatchedVideos(response, month) {
     console.log("Response received", "handleMostWatchedVideos");
     var videos = response.result.rows;
     if (month != undefined) {
-      var nonOrganicVideos = ["vio9VoZRkbQ", "dqkUlrFoZY4", "rNOoyOGBFK4", "Eyvv66xYwS8", "YfrmIjwDvXo", "k40iosW6WWA", "dE2Svgsrex0"];
+      var nonOrganicVideos = ["vio9VoZRkbQ", "dqkUlrFoZY4", "rNOoyOGBFK4",
+          "Eyvv66xYwS8", "YfrmIjwDvXo", "k40iosW6WWA", "dE2Svgsrex0",
+          "5Ci3PlLr8Oo", "G2D82iNV33g"];
       var values = [[month]];
       var index = 0;
       var numVideos = 1;
