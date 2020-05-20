9d49f0e139713fe2f4332e317726422d88de783b
rstrozensky <automationdirectvideo@gmail.com>
2020-05-20 14:28:01 -0400

    Fix logic in thumbnail charts functions

--- resources/js/thumbnailchart.js
+++ resources/js/thumbnailchart.js
@@ -1,24 +1,20 @@
 function displayThumbnails(uploads) {
   var thumbnailContainer = document.getElementById("thumbnail-container");
-  if (!uploads) {
-    loadUploads();
-  } else {
-    var uploadThumbnails = "";
-    uploadThumbnails += `<div class="thumbnail-page">`;
-    for (var i = 0; i < uploads.length; i++) {
-      uploadThumbnails += `
-        <a href="https://youtu.be/${uploads[i]}" target="_blank">
-          <img class="thumbnail"
-              src="https://i.ytimg.com/vi/${uploads[i]}/mqdefault.jpg" 
-              alt="thumbnail"></a>`;
-      if (i % 100 == 99) {
-        uploadThumbnails += `</div><div class="thumbnail-page">`;
-      } else if (i % 10 == 9) {
-        uploadThumbnails += `<br>`;
-      }
+  var uploadThumbnails = "";
+  uploadThumbnails += `<div class="thumbnail-page">`;
+  for (var i = 0; i < uploads.length; i++) {
+    uploadThumbnails += `
+      <a href="https://youtu.be/${uploads[i]}" target="_blank">
+        <img class="thumbnail"
+            src="https://i.ytimg.com/vi/${uploads[i]}/mqdefault.jpg" 
+            alt="thumbnail"></a>`;
+    if (i % 100 == 99) {
+      uploadThumbnails += `</div><div class="thumbnail-page">`;
+    } else if (i % 10 == 9) {
+      uploadThumbnails += `<br>`;
     }
-    thumbnailContainer.innerHTML = uploadThumbnails;
   }
+  thumbnailContainer.innerHTML = uploadThumbnails;
 }
 
 function loadSignedIn() {
@@ -77,5 +73,5 @@ function recordUploads(videoList) {
     let videoId = row[columns["Video ID"]];
     uploads.push(videoId);
   }
-  displayThumbnails(uploads);
+  return uploads;
 }
