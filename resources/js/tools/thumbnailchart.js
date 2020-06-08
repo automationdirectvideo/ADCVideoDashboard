3a5045f54719705cabde02d661aefb782cd8a3b6
rstrozensky <rstrozensky@automationdirect.com>
2020-06-08 14:51:56 -0400

    Update style/variable declarations

--- resources/js/tools/thumbnailchart.js
+++ resources/js/tools/thumbnailchart.js
@@ -16,7 +16,9 @@ function displayThumbnails(uploads) {
       <a href="https://youtu.be/${uploads[i]}" target="_blank">
         <img class="thumbnail"
             src="https://i.ytimg.com/vi/${uploads[i]}/mqdefault.jpg" 
-            alt="thumbnail"></a>`;
+            alt="thumbnail"></a>
+    `;
+    // Splits the thumbnails by pages - 100 per page
     if (i % 100 == 99) {
       uploadThumbnails += `</div><div class="thumbnail-page">`;
     } else if (i % 10 == 9) {
@@ -37,7 +39,7 @@ function loadUploads() {
   requestSpreadsheetData("Input Data", "Video List")
     .then(videoList => {
       let uploads = [];
-      let columns = getColumnHeaders(videoList);
+      const columns = getColumnHeaders(videoList);
       for (let i = videoList.length - 1; i >= 1; i--) {
         const row = videoList[i];
         const videoId = row[columns["Video ID"]];
@@ -57,19 +59,20 @@ function loadUploads() {
  * Loads the page once the user has signed into Google
  */
 function loadSignedIn() {
-  document.getElementById("menu").innerHTML +=
-    `<button class="btn btn-lg btn-primary mt-3" id="refresh-btn">Refresh Thumbnails</button>`;
-  document.getElementById("thumbnail-container").innerHTML =
-    `<h5>No thumbnails are loaded. Press the "Refresh Thumbnails" button to view the thumbnails.</h5>`;
-  document.getElementById("about-list").innerHTML +=
-    `
-      <li>Thumbnails not loading? Try <a href="" id="signout-text">signing out</a> and signing back in.</li>
-      <li>
-        Missing some thumbnails? Make sure the missing videos are listed in the 
-        <a href="https://docs.google.com/spreadsheets/d/1LNVjw5Hf2Ykp89jtxaX9itH5NOoudwaz0T74E7flZZg/edit#gid=485839191?usp=sharing"
-          target="_blank">"ADC Video Dashboard Input Data" Google Sheet</a>.
-      </li>
-    `;
+  document.getElementById("menu").innerHTML += `
+    <button class="btn btn-lg btn-primary mt-3" id="refresh-btn">Refresh Thumbnails</button>
+  `;
+  document.getElementById("thumbnail-container").innerHTML = `
+    <h5>No thumbnails are loaded. Press the "Refresh Thumbnails" button to view the thumbnails.</h5>
+  `;
+  document.getElementById("about-list").innerHTML += `
+    <li>Thumbnails not loading? Try <a href="" id="signout-text">signing out</a> and signing back in.</li>
+    <li>
+      Missing some thumbnails? Make sure the missing videos are listed in the 
+      <a href="https://docs.google.com/spreadsheets/d/1LNVjw5Hf2Ykp89jtxaX9itH5NOoudwaz0T74E7flZZg/edit#gid=485839191?usp=sharing"
+        target="_blank">"ADC Video Dashboard Input Data" Google Sheet</a>.
+    </li>
+  `;
 
   let signoutText = document.getElementById("signout-text");
   signoutText.addEventListener("click", handleSignout);
